65dcc9042b1ce4551fd9852aa720f9996d893fb6
Rui Lopes <rgl@ruilopes.com>
2023-02-21 21:43:37 +0000

    also show the indented saml request xml

--- decode-saml-request-url.py
+++ decode-saml-request-url.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 from urllib.parse import urlparse, parse_qs
 from base64 import b64decode
+import xml.etree.ElementTree as ET
 import zlib
 
 # NB you should replace this value with your own.
@@ -16,4 +17,13 @@ saml_request_deflated = b64decode(saml_request_deflated_base64)
 
 saml_request = zlib.decompress(saml_request_deflated, -15)
 
-print(saml_request.decode('utf-8'))
+saml_request_xml = saml_request.decode('utf-8')
+
+print('SAML Request XML:\n')
+print(saml_request_xml)
+
+print('\nFormatted SAML Request XML:\n')
+saml_request_element = ET.fromstring(saml_request_xml)
+ET.indent(saml_request_element)
+print(ET.tostring(saml_request_element, encoding='unicode'))
+print('\nWARNING: The previous formatted XML is not exactly like the original (e.g. XML prefixes might be different), but it should be semantically equivalent.')
